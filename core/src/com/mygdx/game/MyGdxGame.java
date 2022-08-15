fa9e480110a5588e8f487d409809c9cee30d21f6
LidiaEvmenenko <inu-chan@mail.ru>
2022-08-15 20:33:40 +0500

    Initial commit

---
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -0,0 +1,40 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class MyGdxGame extends ApplicationAdapter {
+	SpriteBatch batch;
+	Texture img;
+	int clk;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		img = new Texture("badlogic.jpg");
+	}
+
+	@Override
+	public void render () {
+		ScreenUtils.clear((float) 0.7, (float) 0.7, (float) 0.7, 1);
+		float x = Gdx.input.getX() - img.getWidth()/2; // х где находится мышка
+		float y = Gdx.graphics.getHeight() - Gdx.input.getY() - img.getHeight()/2;
+		if (Gdx.input.isButtonJustPressed(Input.Buttons.LEFT)) clk++;
+		Gdx.graphics.setTitle("Clicked " + clk + " times");
+		batch.begin();
+	//	batch.draw(img, 0, 0);
+		batch.draw(img, x, y);
+		//batch.draw(img, x*2, 0);
+		batch.end();
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+		img.dispose();
+	}
+}
