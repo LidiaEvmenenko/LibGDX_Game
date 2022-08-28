31e5a7dd59e084020ece68b0fa6c6a6924db9294
LidiaEvmenenko <inu-chan@mail.ru>
2022-08-28 14:11:49 +0500

    HW4: add map and control, adjust setSize() and camera

--- core/src/com/mygdx/game/screens/GameScreen.java
+++ core/src/com/mygdx/game/screens/GameScreen.java
@@ -4,9 +4,14 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.ScreenUtils;
@@ -20,11 +25,29 @@ public class GameScreen implements Screen {
     private Anim animation;
     private Main game;
     private SpriteBatch batch;
+    private OrthographicCamera camera;
+    private TiledMap map;
+    private OrthogonalTiledMapRenderer mapRenderer;
+    private Vector2 mapPosition;
+    private float STEP = 3;
+    private Rectangle mapSize;
 
     public GameScreen(Main game) {
         this.game = game;
         batch = new SpriteBatch();
-        System.out.println("Ggame = "+game);
+        camera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+
+        map = new TmxMapLoader().load("map/карта2.tmx");
+        mapRenderer = new OrthogonalTiledMapRenderer(map);
+
+        map.getLayers().get("объекты").getObjects().getByType(RectangleMapObject.class);//выбор объектов по типу
+        RectangleMapObject tmp = (RectangleMapObject) map.getLayers().get("объекты").getObjects().get("камера");//выбор объектов по имени
+
+        camera.position.x = tmp.getRectangle().x;
+        camera.position.y = tmp.getRectangle().y;
+
+        tmp = (RectangleMapObject) map.getLayers().get("объекты").getObjects().get("граница");
+        mapSize = tmp.getRectangle();
     }
 
     @Override
@@ -36,42 +59,44 @@ public class GameScreen implements Screen {
 
     @Override
     public void render(float delta) {
-
-        if (Gdx.input.isKeyJustPressed(Input.Keys.ANY_KEY)) {
-            System.out.println("batch = "+batch);
+        if (Gdx.input.isKeyJustPressed(Input.Keys.TAB)) {
             dispose();
             game.setScreen(new MenuScreen(this.game));}
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT) &&
+                mapSize.width*2 -animation.getFrame().getRegionWidth() > camera.position.x-1) camera.position.x += STEP;
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT) && mapSize.x+animation.getFrame().getRegionWidth()/2 < camera.position.x+1) camera.position.x -= STEP;
+        if (Gdx.input.isKeyPressed(Input.Keys.UP) && mapSize.y+animation.getFrame().getRegionWidth()/2 < camera.position.y-1) camera.position.y -= STEP;
+        if (Gdx.input.isKeyPressed(Input.Keys.DOWN)
+                && mapSize.height - animation.getFrame().getRegionWidth()/2 > camera.position.y+1) camera.position.y += STEP;
+        if (Gdx.input.isKeyPressed(Input.Keys.P)) camera.zoom += 0.01f;
+        if (Gdx.input.isKeyPressed(Input.Keys.O) && camera.zoom > 0) camera.zoom -= 0.01f;
+        camera.update();
+
         if (Gdx.input.isKeyJustPressed(Input.Keys.L)) { dir = true; }
         if (Gdx.input.isKeyJustPressed(Input.Keys.R)) { dir = false; }
         if (!animation.getFrame().isFlipX() && dir) {
             animation.getFrame().flip(true, false);
-            v.set(-1, 0);
+
         }//налево
         if (animation.getFrame().isFlipX() && !dir) {
             animation.getFrame().flip(true, false);
-            v.set(1, 0);
+
         }//направо
         animation.setTime(Gdx.graphics.getDeltaTime());
         ScreenUtils.clear(0.5F, 0.2f, 1, 1);
+        mapRenderer.setView(camera);
+        mapRenderer.render();
+        batch.setProjectionMatrix(camera.combined);
         batch.begin();
-        batch.draw(animation.getFrame(), pos.x, pos.y);
+        batch.draw(animation.getFrame(), camera.position.x-animation.getFrame().getRegionWidth()/2,
+                camera.position.y-animation.getFrame().getRegionHeight()/2);
         batch.end();
-        pos.add(v);
-        if (pos.x >= Gdx.graphics.getWidth() - animation.getFrame().getRegionWidth()){
-            v.set(-1, 0);
-            animation.getFrame().flip(true, false);
-            dir= !dir;
-        }
-        if (pos.x == 0){
-            v.set(1, 0);
-            animation.getFrame().flip(true, false);
-            dir= !dir;
-        }
     }
 
     @Override
     public void resize(int width, int height) {
-
+        camera.viewportHeight = height;
+        camera.viewportWidth = width;
     }
 
     @Override
